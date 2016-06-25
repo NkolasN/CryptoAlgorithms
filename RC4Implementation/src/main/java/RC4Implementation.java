27f0ca8b8b589ddf172c1d1c8f09c45139cf3fb6
Nikolas <>
2016-06-25 15:14:00 +0100

    ''

--- RC4Implementation/src/main/java/RC4Implementation.java
+++ RC4Implementation/src/main/java/RC4Implementation.java
@@ -2,10 +2,11 @@
 
  /**
   * A toy implementation of RC4.
-  * This code is not to meant be used for securing
-  * information. Use the standard java libraries instead. 
-  * Code provided with no warranties and is not guaranteed
-  * to be free of errors.  
+  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+  * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+  * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM,
+  * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
   * @author NkolasN
   *
   */
