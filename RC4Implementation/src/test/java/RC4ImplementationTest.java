854cb184760ea0021aaaf242074231e3d0d68b37
NkolasN <nikolas.nikiforou92@outlook.com>
2016-07-24 11:41:44 +0100

    Update RC4ImplementationTest.java

--- RC4Implementation/src/test/java/RC4ImplementationTest.java
+++ RC4Implementation/src/test/java/RC4ImplementationTest.java
@@ -14,22 +14,22 @@ import org.junit.Test;
  */
 public class RC4ImplementationTest {
 
-	byte[] plaintext;
-	byte[] key = new byte[16];
+   byte[] plaintext;
+   byte[] key = new byte[16];
 	
-	@Before
-	public void configure(){
-	SecureRandom secureRandom=new SecureRandom();
-	secureRandom.nextBytes(key);
-	plaintext = "hello world!".getBytes();
-	}
-	
-	
-	@Test
-	public void successfulEncryptionDecryption() throws UnsupportedEncodingException{
-	byte[] ciphertext = new RC4Implementation(key).encrypt(plaintext);
-	String decryptedPlaintext = new String(new RC4Implementation(key).decrypt((ciphertext)),"UTF-8");
-	assertEquals("hello world!",decryptedPlaintext);
-	}
+   @Before
+   public void configure(){
+      SecureRandom secureRandom=new SecureRandom();
+      secureRandom.nextBytes(key);
+      plaintext = "hello world!".getBytes();
+   }
+
+
+   @Test
+   public void successfulEncryptionDecryption() throws UnsupportedEncodingException{
+      byte[] ciphertext = new RC4Implementation(key).encrypt(plaintext);
+      String decryptedPlaintext = new String(new RC4Implementation(key).decrypt((ciphertext)),"UTF-8");
+      assertEquals("hello world!",decryptedPlaintext);
+   }
 
 }
